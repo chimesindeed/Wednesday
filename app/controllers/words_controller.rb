40d80b3d29a5256130fa1b9f4d12094d1acd3024
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-06 16:20:13 -0400

    partial for flash messages added. Flash messages implemented in words create method. Partial called in Words new.html.erb

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,12 +1,23 @@
 class WordsController < ApplicationController
 
   def index
+    @words = Word.all
   end
 
   def new
+    @word = Word.new
   end
 
   def create
+  binding.pry #create method was called
+    if @word = Word.new(word_params).save
+      flash[:notice] =  'Word was successfully saved!'
+    else flash[:notice] = "Not saved" 
+    @word = Word.new
+    render new_word_path
+    binding.pry
+    end
+    
   end
   
   def edit
@@ -21,5 +32,8 @@ class WordsController < ApplicationController
   def delete
   end
 
+  def word_params
+    params.require(:word).permit(:name)
+  end
 
 end
