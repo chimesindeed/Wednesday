5e7d049290de0a27a7520c9a233bb5289785af3d
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-07 02:57:24 -0400

    because of new relationship through join table, removed :word_id column from phrases table

--- db/migrate/003_create_phrases.rb
+++ db/migrate/003_create_phrases.rb
@@ -2,7 +2,6 @@ class CreatePhrases < ActiveRecord::Migration[6.0]
   def change
     create_table :phrases do |t|
       t.string :name
-      t.integer :word_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 4) do
 
   create_table "phrases", force: :cascade do |t|
     t.string "name"
-    t.integer "word_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
