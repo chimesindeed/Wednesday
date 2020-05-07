4ec470d2f21d32c275093af08d19a836b354f823
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-06 22:20:28 -0400

    added model validations to letter and word.

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,4 +1,11 @@
 class Letter < ApplicationRecord
   has_many :words
   has_many :phrases, through: :words
+
+  validates :name,
+            format: { with: /\A[a-zA-Z]+\z/ },
+            uniqueness: true,
+            allow_blank: false,
+            allow_nil: false,
+            length: { is: 1 }
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,12 @@
+# Word Model
 class Word < ApplicationRecord
   belongs_to :letter
   has_many :phrases
+
+  validates :name,
+            format: { with: /\A[a-zA-Z]+\z/ },
+            uniqueness: true,
+            allow_blank: false,
+            allow_nil: false,
+            length: { minimum: 2, maximum: 25 }
 end
