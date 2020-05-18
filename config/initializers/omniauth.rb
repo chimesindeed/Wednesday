ca5ab53bb0c37ca242c1a341e38f9c4134de720e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-18 06:23:07 -0400

    redid login / sessions to work with devise and omniauth

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['e2f0974af3f54635e429'], ENV['0b8fe305fafd3e6725653bda2340e531fd755d9a']
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 #
 Rails.application.routes.draw do
   
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: 'butterrolls#main'
 
-  resources :sessions, only: %i[new create destroy]
   
   get 'main', :to => 'butterrolls#main', as: 'main'
   get 'about', :to => 'butterrolls#about', as: 'about'
   
-  get 'signup', :to => 'users#new', as: 'signup'
-  get 'login', :to => 'sessions#new', as: 'login'
-  post 'login', :to => 'sessions#create', as: 'verified'
-  post 'logout', :controller =>'sessions', :action => 'destroy', :method => 'delete'
-  resource :session
-  resources :users
-  post 'users', :to => 'users#create'
   resources :words
   resources :phrases
   resources :matches
