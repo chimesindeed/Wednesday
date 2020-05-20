855483bf1ce1b7ecfb7e5c15eb6fc1c95491b1ed
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-05-19 23:23:07 -0400

    general tweaking of models and views, nothing major

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,4 @@
+# /app/models/application_record.rb
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-    
-   # validates :name,
-               # format: { with: /\A[a-zA-Z]+\z/ },
-               # allow_blank: false
-  
-  # before_save { self.name ? name.downcase! : return  }
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,9 @@
-# Word Model
+# /app/models/word.rb
 class Word < ApplicationRecord
   belongs_to :user
   has_many :phrases, through: :matches
   validates :name,
-            length: { minimum: 2, maximum: 25 }
-
+            length: { minimum: 2, maximum: 25 },
+            format: { with: /\A[a-zA-Z]+\z/ },
+            allow_blank: false
 end
