6a705098460b5ae708bb28cb17973aab3e2c1d47
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-02 06:57:14 -0400

    added model validations for Phrase Model.  Removed allow blank validation from Word Model because covered by length validation

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,4 +1,8 @@
 class Phrase < ApplicationRecord
   belongs_to :user
   has_many :words, through: :matches
+  
+  validates  :name,
+             length: { minimum: 4, maximum: 25, :message => 'must be between 4 and 25 characters' },
+             format: { with: /\A[a-zA-Z]+\z/, :message => 'only letters please' }
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,14 +1,14 @@
 # /app/models/word.rb
 class Word < ApplicationRecord
   belongs_to :user
-  has_many :phrases, through: :matches
-  validates :name,
-            length: { minimum: 2, maximum: 25 },
-            format: { with: /\A[a-zA-Z]+\z/ },
-            allow_blank: false
-            before_save { self.name.downcase!}
-  
+  has_many   :phrases, through: :matches
 
+  validates  :name,
+             length: { minimum: 2, maximum: 25, :message => 'must be between 2 and 25 characters' },
+             format: { with: /\A[a-zA-Z]+\z/, :message => 'only letters please' }
+
+             before_save { self.name.downcase! }
+  
   def self.order_words
    Word.order(:name)
   end
